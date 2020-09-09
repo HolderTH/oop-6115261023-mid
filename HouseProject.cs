9ee2374199bf26a3874aaaa1788b8176271292ac
HolderTH <68211127+HolderTH@users.noreply.github.com>
2020-09-09 12:06:12 +0700

    Add files via upload

---
+++ HouseProject.cs
@@ -0,0 +1,31 @@
+﻿class HouseProject
+{
+    private string name;
+    private House[] houses = new House[10];
+    public HouseProject(string name, House[] houses)
+    {
+        this.name = name;
+        this.houses = houses;
+    }
+    public string Name { get => name; set => name = value; }
+    internal House[] Houses { get => houses; set => houses = value; }
+    public void setHouse(House h, int index)
+    {
+        this.houses[index] = h;
+    }
+    public House GetHouse(int index)
+    {
+        return this.houses[index];
+    }
+    public void listAllHouse()
+    {
+        for (int i = 0; i < this.houses.Length; i++)
+        {
+            if (this.houses[i] != null)
+            {
+                Console.WriteLine(this.houses[i].ToString());
+            }
+        }
+    }
+}
+}
---
+++ Village.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace oop_mid_6115261023
+{
+    class Village
+    {
+        private string name;
+        private int val;
+        public Village(string name, int val)
+        {
+            this.name = name;
+            this.val = val;
+        }
+        public string Name { get => name; set => name = value; }
+        public int Val { get => val; set => val = value; }
